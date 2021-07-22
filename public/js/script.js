7081b9278cf598ade4e3e434beb55a2394467de9
Jerome <instarz@hotmail.fr>
2021-07-22 11:15:38 +0200

    jquery added, navbar animate on click, page join front ok

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
 
+    let $containerNav = $('.container-nav'),
+    $logo = $('.container-logo'),
+    $logoArrow = $('#arrow-log'),
+    $menuLogo = $('.menu-logo'),
+    $bgImageHome = $('#bg-img');
+
+    $containerNav.css({"display":"none"});
+
+    $logo.on('click', function(){
+        $containerNav.animate({height:"toggle"})
+        $menuLogo.animate({fontSize: "0px"}, 1000)
+    });
 
 });
